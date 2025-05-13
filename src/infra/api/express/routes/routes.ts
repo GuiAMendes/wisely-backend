a29bc633e537b9303622ea701c236578b1a768a0
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-13 17:06:04 -0300

    feat: create interface to define methods in a route of use Express

---
+++ src/infra/api/express/routes/routes.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+
+export type HttpMethod = "get" | "post" | "put" | "patch" | "delete";
+
+export const HttpMethod = {
+  GET: "get" as HttpMethod,
+  POST: "post" as HttpMethod,
+  PUT: "put" as HttpMethod,
+  PATCH: "patch" as HttpMethod,
+  DELETE: "delete" as HttpMethod,
+} as const;
+
+export interface Route {
+  getMiddlewares(): Array<
+    (req: Request, res: Response, next: NextFunction) => void
+  >;
+  getHandler(): (req: Request, res: Response) => Promise<void>;
+  getPath(): string;
+  getMethod(): HttpMethod;
+}
