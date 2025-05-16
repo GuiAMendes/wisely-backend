be1894aa4c72d133d02ae0db35f6e1c920f3c724
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-16 17:23:01 -0300

    feat: add create jwt service

---
+++ src/infra/services/token/interfaces/token.interfaces.ts
@@ -0,0 +1,16 @@
+// External libraries
+import { SignOptions, VerifyOptions, JwtPayload } from "jsonwebtoken";
+
+export interface TokenProvider {
+  sign(
+    payload: string | Buffer | object,
+    secret: string,
+    options?: SignOptions
+  ): string;
+
+  verify(
+    token: string,
+    secret: string,
+    options?: VerifyOptions
+  ): JwtPayload | string;
+}
