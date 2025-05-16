9d7e1d45b1acb2b21f64ac330d7e13aa67c82c66
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-16 10:14:29 -0300

    refactor: separete regex in constant package and improve readiability of code in validators

--- src/domain/fileModel/validator/MetaData.validator.ts
+++ src/domain/fileModel/validator/MetaData.validator.ts
@@ -2,17 +2,15 @@
 import { isEmpty, isSafe } from "../../../shared/validators";
 
 // Constants
-import { MAX_BASE64_SIZE } from "../constants";
+import { BASE64_REGEX, MAX_BASE64_SIZE } from "../constants";
 
 export class MetaDataValidator {
   public static ensureBase64Valid(content: string) {
-    if (!isSafe(content))
+    if (!isSafe(content)) {
       throw new Error("O conteúdo indicado é inválido ou inseguro.");
+    }
 
-    const base64Regex =
-      /^(?:[A-Z0-9+\/]{4})*(?:[A-Z0-9+\/]{2}==|[A-Z0-9+\/]{3}=)?$/i;
-
-    if (!base64Regex.test(content)) {
+    if (!BASE64_REGEX.test(content)) {
       throw new Error(
         "O conteúdo do arquivo deve estar em formato base64 válido."
       );
@@ -20,10 +18,12 @@ export class MetaDataValidator {
   }
 
   public static ensureContentWithinLimit(content: string) {
-    if (isEmpty(content))
+    if (isEmpty(content)) {
       throw new Error("O conteúdo indicado é inválido ou inseguro.");
+    }
 
-    if (content.length > MAX_BASE64_SIZE)
+    if (content.length > MAX_BASE64_SIZE) {
       throw new Error("O conteúdo do arquivo execede o limite de 5MB.");
+    }
   }
 }
