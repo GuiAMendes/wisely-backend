a0f2875c7f76f7a1f0db590624b72d553170515d
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-17 10:05:27 -0300

    refactor: change method of interface gateway to deactivate user

--- src/domain/user/gateway/user.gateway.ts
+++ src/domain/user/gateway/user.gateway.ts
@@ -6,5 +6,5 @@ export interface UserGateway {
   findById(id: string): Promise<User | null>;
   findByEmail(email: string): Promise<User | null>;
   update(user: User): Promise<User>;
-  desactive(id: string): Promise<User>;
+  deactivate(id: string): Promise<User>;
 }
