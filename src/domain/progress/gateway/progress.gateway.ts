1a297c178445de3f1846d81d9808cfa8000695f8
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-17 11:22:27 -0300

    feat: create progress gateway

---
+++ src/domain/progress/gateway/progress.gateway.ts
@@ -0,0 +1,11 @@
+// Entity
+import { Progress } from "../entity";
+
+export interface ProgressGateway {
+  create(progress: Progress): Promise<Progress>;
+  update(progress: Progress): Promise<Progress>;
+  findById(id: string): Promise<Progress | null>;
+  findByJourneyId(journeyId: string): Promise<Progress | null>;
+  resumeStatistics(idUser: string): Promise<Progress[]>;
+  deactivate(id: string): Promise<Progress>;
+}
