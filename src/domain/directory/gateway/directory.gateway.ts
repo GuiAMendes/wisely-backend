1d83e8196303e2fb0fd36d910684578f26855b7d
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-17 10:17:01 -0300

    feat: create directory gateway

---
+++ src/domain/directory/gateway/directory.gateway.ts
@@ -0,0 +1,14 @@
+// Entity
+import { Directory } from "../entity";
+
+export interface DirectoryGateway {
+  create(directory: Directory): Promise<Directory>;
+  findById(id: string): Promise<Directory | null>;
+  findByUser(idUser: string): Promise<Directory[]>;
+  findByName(directoryName: string): Promise<Directory[]>;
+  listAll(idUser: string): Promise<Directory[]>;
+  listRecentAccess(idUser: string): Promise<Directory[]>;
+  update(directory: Directory): Promise<Directory>;
+  complete(id: string): Promise<Directory>;
+  deactivate(id: string): Promise<Directory>;
+}
