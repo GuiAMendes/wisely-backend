4c01bb5d06aad8f4f37fcce52a41dec377eba4c8
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-17 11:29:38 -0300

    feat: create summary gateway

---
+++ src/domain/summary/gateway/summary.gateway.ts
@@ -0,0 +1,11 @@
+// Entity
+import { Summary } from "../entity";
+
+export interface SummaryGateway {
+  create(Summary: Summary): Promise<Summary>;
+  update(Summary: Summary): Promise<Summary>;
+  findById(id: string): Promise<Summary | null>;
+  findByTopic(topicId: string): Promise<Summary | null>;
+  complete(id: string): Promise<Summary>;
+  deactivate(id: string): Promise<Summary>;
+}
