f1bdb93a34c890cc3e7a1bb98f6829041e4fb252
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-20 23:08:19 -0300

    fix: params of method list all

--- src/domain/gateway/directory/directory.gateway.ts
+++ src/domain/gateway/directory/directory.gateway.ts
@@ -4,10 +4,10 @@ import { Directory } from "../../entity/directory/Directory";
 export interface DirectoryGateway {
   create(directory: Directory): Promise<void>;
   findById(id: string): Promise<Directory | null>;
-  findByName(directoryName: string): Promise<Directory[]>;
+  findByName(idUser: string, directoryName: string): Promise<Directory[]>;
   listAll(idUser: string): Promise<Directory[]>;
   listRecentAccess(idUser: string): Promise<Directory[]>;
-  update(directory: Directory): Promise<void>;
+  updateName(id: string, newName: string): Promise<void>;
   complete(id: string): Promise<void>;
   deactivate(id: string): Promise<void>;
 }
