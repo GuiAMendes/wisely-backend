8b183bd51d2420cbd55b6b32a59ad20de2023419
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-22 02:41:53 -0300

    feat: create deactivate directory dtos

---
+++ src/presentation/dtos/directory/deactivate/controllersDTO.ts
@@ -0,0 +1,8 @@
+export type DeactivateDirectoryControllerInputDTO = {
+  idDirectory: string;
+};
+
+export type DeactivateDirectoryControllerOutputDTO = {
+  idDirectory: string;
+  status: boolean;
+};
