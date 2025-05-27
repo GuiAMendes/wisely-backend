df2fbcf948342d3076200931fa394d6f4569d468
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-27 20:11:15 -0300

    feat: create settings dtos

---
+++ src/presentation/dtos/settings/create/controllersDTO.ts
@@ -0,0 +1,7 @@
+export type CreateSettingsControllerInputDTO = {
+  idUser: string;
+};
+
+export type CreateSettingsControllerOutputDTO = {
+  idUser: string;
+};
---
+++ src/presentation/dtos/settings/create/usecaseDTO.ts
@@ -0,0 +1,7 @@
+export type CreateSettingsUseCaseInputDTO = {
+  idUser: string;
+};
+
+export type CreateSettingsUseCaseOutputDTO = {
+  idUser: string;
+};
