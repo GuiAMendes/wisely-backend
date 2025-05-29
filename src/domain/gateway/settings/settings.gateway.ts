f54714041d7b58cc5f486db4442305e94948c278
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-29 13:15:17 -0300

    refactor: adjust type of method in gateway

--- src/domain/gateway/settings/settings.gateway.ts
+++ src/domain/gateway/settings/settings.gateway.ts
@@ -1,5 +1,10 @@
 export interface SettingsGateway {
   create(idUser: string): Promise<void>;
-  findByIdUser(idUser: string): Promise<string | null>;
-  updateColorSchema(idUser: string, newColorShema: string): Promise<void>;
+  findByIdUser(
+    idUser: string
+  ): Promise<{ primaryColor: string; secondaryColor: string } | null>;
+  updateColorSchema(
+    idUser: string,
+    newColorShema: { primaryColor: string; secondaryColor: string }
+  ): Promise<void>;
 }
