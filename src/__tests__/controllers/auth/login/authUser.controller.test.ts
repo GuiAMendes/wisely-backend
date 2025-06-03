c253f0e9a4a2fb701fa3f08eb480be1e083fb9c2
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-06-03 18:32:18 -0300

    test: create test of integration from login and register user

--- src/__tests__/controllers/auth/login/authUser.controller.test.ts
+++ src/__tests__/controllers/auth/login/authUser.controller.test.ts
@@ -2,9 +2,10 @@ import { createUserControllers } from "../../../../factories/userFactory";
 import { Request, Response } from "express";
 import { prisma } from "../../../../shared/factory/sharedFactory";
 
-describe("CreateUserController", () => {
-  it("Should create a new user successfully", async () => {
-    const [, createUser] = createUserControllers();
+describe("AuthUserController", () => {
+  it("Should create a new token successfully", async () => {
+    const [authUser, createUser] = createUserControllers();
+
     const json = jest.fn();
     const status = jest.fn(() => ({ json }));
 
@@ -12,21 +13,33 @@ describe("CreateUserController", () => {
     const password = "DummyW@rd0310";
     const username = `dummy_user_${Date.now()}`;
 
+    await createUser.getHandler()(
+      {
+        body: { username, email, password },
+      } as Request,
+      {
+        status,
+      } as unknown as Response
+    );
+
+    json.mockClear();
+    status.mockClear();
+
     const req = {
-      body: { username, email, password },
+      body: { email, password },
     } as Request;
 
     const res = {
       status,
     } as unknown as Response;
 
-    const handler = createUser.getHandler();
+    const handler = authUser.getHandler();
     await handler(req, res);
 
-    expect(status).toHaveBeenCalledWith(201);
+    expect(status).toHaveBeenCalledWith(200);
     expect(json).toHaveBeenCalledWith(
       expect.objectContaining({
-        id: expect.any(String),
+        token: expect.any(String),
       })
     );
 
