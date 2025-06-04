b340c85bf889a3dfcf99430afff6c612b6d58842
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-06-04 18:07:12 -0300

    feat: create rename title of summary dtos

---
+++ src/presentation/dtos/summary/renameTitle/controllersDTO.ts
@@ -0,0 +1,9 @@
+export type RenameTitleOfSummaryControllerInputDTO = {
+  idSummary: string;
+  title: string;
+};
+
+export type RenameTitleOfSummaryControllerOutputDTO = {
+  idSummary: string;
+  newTitle: string;
+};
