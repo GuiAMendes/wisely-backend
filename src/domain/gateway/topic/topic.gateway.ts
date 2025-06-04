68b1deb5d792d3a927cb6fd45b08b3b0493a094f
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-06-04 00:08:56 -0300

    feat: create topic gateway

--- src/domain/gateway/topic/topic.gateway.ts
+++ src/domain/gateway/topic/topic.gateway.ts
@@ -3,10 +3,10 @@ import { Topic } from "../../entity/topic/Topic";
 
 export interface TopicGateway {
   create(topic: Topic): Promise<void>;
-  update(topic: Topic): Promise<void>;
   findById(id: string): Promise<Topic | null>;
-  findByName(topicName: string): Promise<Topic[]>;
+  findByName(idJourney: string, topicName: string): Promise<Topic[]>;
   listAll(idJourney: string): Promise<Topic[]>;
   complete(id: string): Promise<void>;
   deactivate(id: string): Promise<void>;
+  updateName(id: string, newName: string): Promise<void>;
 }
