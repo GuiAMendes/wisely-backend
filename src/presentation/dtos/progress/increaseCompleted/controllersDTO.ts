a77ca3ff36ef4228edb59c65aaa9be3953c9ab35
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-06-03 21:25:01 -0300

    feat: create increse progress dtos

---
+++ src/presentation/dtos/progress/increaseCompleted/controllersDTO.ts
@@ -0,0 +1,11 @@
+export type IncreaseProgressControllerInputDTO = {
+  idJourney: string;
+  completedTopics: number;
+  totalTopics: number;
+};
+
+export type IncreaseProgressControllerOutputDTO = {
+  idJourney: string;
+  completedTopics: number;
+  totalTopics: number;
+};
