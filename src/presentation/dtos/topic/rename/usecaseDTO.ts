2489bbc7f632f1076d3713cc9e186e1cf307a0ba
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-06-04 11:44:39 -0300

    feat: create rename topic dtos

---
+++ src/presentation/dtos/topic/rename/usecaseDTO.ts
@@ -0,0 +1,9 @@
+export type RenameTopicUseCaseInputDTO = {
+  idTopic: string;
+  newTopicName: string;
+};
+
+export type RenameTopicUseCaseOutputDTO = {
+  idTopic: string;
+  topicName: string;
+};
