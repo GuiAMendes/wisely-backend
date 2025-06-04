56b2591f07878398fe085a42c882b2eedee87b26
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-06-04 14:28:49 -0300

    feat: create summary dtos

---
+++ src/presentation/dtos/summary/create/controllersDTO.ts
@@ -0,0 +1,9 @@
+export type CreateSummaryControllerInputDTO = {
+  idTopic: string;
+  title: string;
+  noteContent: string;
+};
+
+export type CreateSummaryControllerOutputDTO = {
+  id: string;
+};
